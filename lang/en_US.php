102ddcf7eee6ec2b92d935acbf4ca4e85a463e49
Sean Harvey <sean@silverstripe.com>
2007-11-06 06:00:18 +0000

    Moved OpenID to module

---
+++ lang/en_US.php
@@ -0,0 +1,21 @@
+<?php
+
+global $lang;
+
+$lang['en_US']['Security']['OPENIDHEADER'] = 'OpenID/i-name credentials';
+$lang['en_US']['Security']['OPENIDDESC'] = '<p>Make sure you enter your normalized OpenID/i-name credentials 
+				here, i.e. with protocol and trailing slash for OpenID (e.g. http://openid.silverstripe.com/).</p>';
+$lang['en_US']['Security']['OPENIDURL'] = 'OpenID URL/i-name';
+$lang['en_US']['Security']['MEMBERALREADYEXISTS'] = 'There already exists a member with this identity URL';
+$lang['en_US']['OpenIDAuthenticator']['ERRORCRED'] = 'Please enter your OpenID URL or your i-name.';
+$lang['en_US']['OpenIDAuthenticator']['TRANSACTIONINPROGRESS'] = 'OpenID transaction in progress';
+$lang['en_US']['OpenIDAuthenticator']['VERIFICATIONCANCELLED'] = 'The verification was cancelled. Please try again.';
+$lang['en_US']['OpenIDAuthenticator']['AUTHFAILED'] = 'The OpenID/i-name authentication failed.';
+$lang['en_US']['OpenIDAuthenticator']['LOGINFAILED'] = 'Login failed. Please try again.';
+$lang['en_US']['OpenIDLoginForm']['DESC'] = '<div id="OpenIDDescription"><p>OpenID is an Internet-wide identity system
+		  					that allows you to sign in to many websites with a single account.
+							For more information visit <a href="http://openid.net">openid.net</a>.</p></div>
+						';
+$lang['en_US']['OpenIDLoginForm']['URL'] = 'OpenID URL';
+
+?>
---
+++ lang/zh_CN.php
@@ -0,0 +1,21 @@
+<?php
+
+i18n::include_locale_file('sapphire', 'en_US');
+
+global $lang;
+
+if(!isset($lang['zh_CN'])) $lang['zh_CN'] = $lang['en_US'];
+
+$lang['zh_CN']['Security']['OPENIDHEADER'] = '开放系统身份（OpenID）/i-name登录信息';
+$lang['zh_CN']['Security']['OPENIDDESC'] = '<p>请确认您在此输入您标准化的开放系统身份（OpenID）/i-name登录信息，亦即,带有协议及尾随斜杠（例如，http://openid.silverstripe.com/）。</p>';
+$lang['zh_CN']['Security']['OPENIDURL'] = '开放系统身份（OpenID）的URL/i-name';
+$lang['zh_CN']['Security']['MEMBERALREADYEXISTS'] = '已有会员占用了此身份认证URL';
+$lang['zh_CN']['OpenIDAuthenticator']['ERRORCRED'] = '请输入您的开放系统身份（OpenID）或您的i-name';
+$lang['zh_CN']['OpenIDAuthenticator']['TRANSACTIONINPROGRESS'] = '开放系统身份（OpenID）正在认证中';
+$lang['zh_CN']['OpenIDAuthenticator']['VERIFICATIONCANCELLED'] = '验证被取消。请重试。';
+$lang['zh_CN']['OpenIDAuthenticator']['AUTHFAILED'] = '开放系统身份（OpenID）/i-name认证失败。';
+$lang['zh_CN']['OpenIDAuthenticator']['LOGINFAILED'] = '登录失败。请重试。';
+$lang['zh_CN']['OpenIDLoginForm']['DESC'] = '<div id="开放系统身份描述“><p>开放系统身份是一个互联网范围内的身份认证系统，他允许您用单一帐户注册登录许多网站。更多信息，访问<a href="http://openid.net">openid.net</a>.</p></div>';
+$lang['zh_CN']['OpenIDLoginForm']['URL'] = '开放系统身份（OpenID）的URL';
+
+?>
