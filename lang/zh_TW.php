d3ba3261e2d4b3c5d3cb5b287b104f95262b7039
Ingo Schommer <ingo@silverstripe.com>
2007-12-17 02:40:03 +0000

    Merged revisions 47179 via svnmerge from http://svn.silverstripe.com/open/modules/auth_openid/trunk
    ........
      r47179 | ischommer | 2007-12-17 15:18:28 +1300 (Mon, 17 Dec 2007) | 1 line
      
      Added chinese (taiwan) (thanks to jeff and normann!)
    ........

---
+++ lang/zh_TW.php
@@ -0,0 +1,21 @@
+<?php
+
+i18n::include_locale_file('auth_openid', 'en_US');
+
+global $lang;
+
+$lang['zh_TW'] = $lang['en_US'];
+
+$lang['zh_TW']['OpenIDAuthenticator']['AUTHFAILED'] = 'OpenID/i-name 身分驗證失敗。';
+$lang['zh_TW']['OpenIDAuthenticator']['ERRORCRED'] = '請輸入您的OpenID 身分或i-name。';
+$lang['zh_TW']['OpenIDAuthenticator']['LOGINFAILED'] = '登入失敗。再試一次。';
+$lang['zh_TW']['OpenIDAuthenticator']['TRANSACTIONINPROGRESS'] = 'OpenID正在溝通';
+$lang['zh_TW']['OpenIDAuthenticator']['VERIFICATIONCANCELLED'] = '身分驗證已被取消。請再試一次。';
+$lang['zh_TW']['OpenIDLoginForm']['DESC'] = '<div id="OpenIDDescription"><p>OpenID 是一個網路通用的身分認證系統。它能夠讓您用一個帳戶登入許多網站。詳情請至<a href="http://openid.net">openid.net 網站</a>.</p></div>';
+$lang['zh_TW']['OpenIDLoginForm']['URL'] = 'OpenID身分';
+$lang['zh_TW']['Security']['MEMBERALREADYEXISTS'] = '已經有人使用這個身分';
+$lang['zh_TW']['Security']['OPENIDDESC'] = '<p>f請確認您輸入的OpenID/i-name身分是正式的。例如：OpenID含有通訊協定及結尾斜線 (http://openid.silverstripe.com/)</p>';
+$lang['zh_TW']['Security']['OPENIDHEADER'] = 'OpenID/i-name 身分登入';
+$lang['zh_TW']['Security']['OPENIDURL'] = 'OpenID 身分/i-name';
+
+?>
