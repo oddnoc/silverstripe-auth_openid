ba96039508e414e7f2f186e0870ac14de34be684
Ingo Schommer <ingo@silverstripe.com>
2008-03-11 00:49:16 +0000

    Merged revisions 50844 via svnmerge from http://svn.silverstripe.com/open/modules/auth_openid/trunk
    ........
      r50844 | ischommer | 2008-03-11 13:43:07 +1300 (Tue, 11 Mar 2008) | 1 line
      
      updated en_US master entities
    ........

--- lang/en_US.php
+++ lang/en_US.php
@@ -4,29 +4,27 @@ i18n::include_locale_file('auth_openid', 'en_US');
 
 global $lang;
 
-$lang['en_US'] = $lang['en_US'];
+global $lang;
 
-$lang['en_US']['OpenIDLoginForm']['URL'] = 'OpenID URL';
-$lang['en_US']['OpenIDAuthenticator']['LOGINFAILED'] = 'Login failed. Please try again.';
+$lang['en_US']['Member']['BUTTONADDOPENIDTOACCOUNT'] = 'Add my OpenID to this account';
+$lang['en_US']['Member']['OPENIDADDEDTOACCOUNT'] = 'Your OpenID was added to your account, you can use it now to log in.';
+$lang['en_US']['Security']['OPENIDHEADER'] = 'OpenID/i-name credentials';
+$lang['en_US']['Security']['EDITOPENIDURL'] = 'OpenID URL/i-name (e.g. http://openid.silverstripe.com/)';
+$lang['en_US']['Security']['MEMBERALREADYEXISTS'] = 'There already exists a member with this identity URL';
+$lang['en_US']['OpenIDAuthenticator']['ERRORCRED'] = 'Please enter your OpenID URL or your i-name.';
+$lang['en_US']['OpenIDAuthenticator']['TRANSACTIONINPROGRESS'] = 'OpenID transaction in progress';
+$lang['en_US']['OpenIDAuthenticator']['VERIFICATIONCANCELLED'] = 'The verification was cancelled. Please try again.';
+$lang['en_US']['OpenIDAuthenticator']['AUTHFAILED'] = 'The OpenID/i-name authentication failed.';
+$lang['en_US']['OpenIDAuthenticator']['LOGINFAILED'] = 'This OpenID (%s) is not registered to any account on this website. <a href="%s">Click here to add your OpenID to your account</a>.';
+$lang['en_US']['OpenIDAuthenticator']['ADDOPENIDTOACCOUNTHEADER'] = 'Add your OpenID to your account';
+$lang['en_US']['OpenIDAuthenticator']['NOOPENIDPASSED'] = 'Login with your OpenID to add it to your account.';
+$lang['en_US']['OpenIDAuthenticator']['NOTEADDOPENIDTOACCOUNT'] = 'Enter your e-mail address and your password to add your OpenID to your account.';
 $lang['en_US']['OpenIDLoginForm']['DESC'] = '<div id="OpenIDDescription"><p>OpenID is an Internet-wide identity system
 		  					that allows you to sign in to many websites with a single account.
-							For more information visit <a href="http://openid.net">openid.net</a>.</p></div>
+							For more information visit <a href="http://openid.net">OpenID.net</a>.</p>
+							<p>If you want to associate your OpenID with your existing account on this website,
+							  simple log in here with your OpenID and follow the instructions.</div>
 						';
-$lang['en_US']['OpenIDAuthenticator']['AUTHFAILED'] = 'The OpenID/i-name authentication failed.';
-$lang['en_US']['OpenIDAuthenticator']['VERIFICATIONCANCELLED'] = 'The verification was cancelled. Please try again.';
-$lang['en_US']['OpenIDAuthenticator']['TRANSACTIONINPROGRESS'] = 'OpenID transaction in progress';
-$lang['en_US']['OpenIDAuthenticator']['ERRORCRED'] = 'Please enter your OpenID URL or your i-name.';
-$lang['en_US']['Security']['MEMBERALREADYEXISTS'] = 'There already exists a member with this identity URL';
-$lang['en_US']['Security']['OPENIDURL'] = 'OpenID URL/i-name';
-$lang['en_US']['Security']['OPENIDDESC'] = '<p>Make sure you enter your normalized OpenID/i-name credentials 
-				here, i.e. with protocol and trailing slash for OpenID (e.g. http://openid.silverstripe.com/).</p>';
-$lang['en_US']['Security']['OPENIDHEADER'] = 'OpenID/i-name credentials';
-
-// New2
-
-$lang['en_US']['Security']['EDITOPENIDURL'] = 'OpenID URL/i-name (e.g. http://openid.silverstripe.com/)';
-$lang['en_US']['Security']['OPENIDURLNORMALIZATION'] = '<p>Make sure you enter your normalized OpenID/i-name credentials 
-				here, i.e. with protocol and trailing slash for OpenID (e.g. http://openid.silverstripe.com/).</p>';
-
+$lang['en_US']['OpenIDLoginForm']['URL'] = 'OpenID URL';
 
 ?>
